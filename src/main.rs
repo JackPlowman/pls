62ac1d9929e704641b757d78873059c72acb5e99
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-12-29 22:34:56 +0000

    List folders in Working Directory (#4)
    # Pull Request
    
    ## Description
    
    This pull request introduces several enhancements to the terminal UI
    application in `src/main.rs`. The most significant changes include the
    addition of a new function to run the application, improvements to the
    terminal setup and restoration process, and the implementation of a
    folder list display.
    
    ### Enhancements to terminal UI application:
    
    * **Refactoring and Error Handling:**
    * Changed the return type of `main` from `Result<(), io::Error>` to
    `Result<(), Box<dyn Error>>` to handle a broader range of errors.
      * Added error handling to print errors if the application run fails.
    
    * **New Functionality:**
    * Introduced a new function `run_app` to encapsulate the main
    application loop and rendering logic. This function continuously reads
    events and updates the terminal UI.
    
    * **UI Improvements:**
    * Implemented a folder list display using `tui` widgets. The list shows
    folder names from the current directory, with a highlighted selection
    and a custom style.
    * Utilized `tui` layout components to organize the UI elements within
    the terminal.
    
    These changes enhance the application's robustness, readability, and
    user interface, providing a more structured and interactive terminal
    experience.
    
    Fixes #51

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,29 @@
 use crossterm::{
-    event::{DisableMouseCapture, EnableMouseCapture},
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use std::{io, thread, time::Duration};
+use std::fs;
+use std::{error::Error, io};
 use tui::{
-    backend::CrosstermBackend,
-    widgets::{Block, Borders},
+    backend::{Backend, CrosstermBackend},
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    widgets::{Block, Borders, List, ListItem},
     Terminal,
 };
 
-fn main() -> Result<(), io::Error> {
-    // setup terminal
+fn main() -> Result<(), Box<dyn Error>> {
+    // Set up terminal
     enable_raw_mode()?;
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    terminal.draw(|f| {
-        let size = f.size();
-        let block = Block::default().title("Block").borders(Borders::ALL);
-        f.render_widget(block, size);
-    })?;
+    let result = run_app(&mut terminal);
 
-    thread::sleep(Duration::from_millis(5000));
-
-    // restore terminal
+    // Restore terminal
     disable_raw_mode()?;
     execute!(
         terminal.backend_mut(),
@@ -35,5 +32,56 @@ fn main() -> Result<(), io::Error> {
     )?;
     terminal.show_cursor()?;
 
+    if let Err(err) = result {
+        println!("{:?}", err)
+    }
+
     Ok(())
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
+    loop {
+        terminal.draw(|f| {
+            let size = f.size();
+
+            // Create a list of folder names
+            let folders = fs::read_dir(".")
+                .unwrap()
+                .filter_map(Result::ok)
+                .filter_map(|entry| {
+                    entry
+                        .file_type()
+                        .ok()
+                        .filter(|ft| ft.is_dir())
+                        .map(|_| entry)
+                })
+                .filter_map(|entry| entry.file_name().into_string().ok())
+                .collect::<Vec<_>>();
+
+            let items: Vec<ListItem> = folders.iter().map(|i| ListItem::new(i.as_ref())).collect();
+            let list = List::new(items)
+                .block(Block::default().borders(Borders::ALL).title("Folders"))
+                .style(Style::default().fg(Color::White))
+                .highlight_style(
+                    Style::default()
+                        .bg(Color::LightGreen)
+                        .add_modifier(Modifier::BOLD),
+                )
+                .highlight_symbol(">> ");
+
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(1)
+                .constraints([Constraint::Percentage(100)].as_ref())
+                .split(size);
+
+            f.render_widget(list, chunks[0]);
+        })?;
+
+        if let Event::Key(key) = event::read()? {
+            if key.code == KeyCode::Char('q') {
+                return Ok(());
+            }
+        }
+    }
 }
